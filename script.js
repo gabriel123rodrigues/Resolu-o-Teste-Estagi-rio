5dcc113bb534365978f505a1febade7692a78915
Gabriel <gabriel123rodrigues@hotmail.com>
2024-02-27 15:15:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const elementos ={};
-function buscaElementos(){
 
+function buscaElementos(){
+    const elementos ={};
     const titulo = document.querySelector('.title-1');
-    titulo ? elementos["Nome"] = titulo.innerText : elementos['nome'] = 'não existe' ;
+    elementos["Nome"] = titulo.innerText ;
 
     document.querySelectorAll('#field_categorias,#field_brands,#field_generic_name, #field_quantity, #field_packaging').forEach(elemento =>{
         const key = elemento.querySelector('.field');
@@ -20,18 +20,55 @@ function buscaElementos(){
         }
         
     });
-    const atributos = document.querySelectorAll('.attr_text');
 
+    const atributos = document.querySelectorAll('.attr_text');
     atributos.forEach(atributo=>{
         if(atributo){
             const titulo = atributo.querySelector('.attr_title');
             const texto = atributo.querySelector('span')
             elementos [titulo.innerText] = texto.innerText;
         } else {
-            const addKey = 'elemento nao existe';
+            const addKey = null;
             elementos[addKey] = addKey
         }
+    });
+
+    const ingredientes = document.querySelectorAll('#panel_ingredients_content');
+    ingredientes.forEach(ingrediente=>{
+        if(ingrediente){
+            const lista = ingrediente.querySelector('.panel_text').innerText;
+            const arrayIngredientes = lista.split(',').map(item=> item.trim())
+            elementos ['Ingredientes'] = arrayIngredientes;
+        } else {
+            elementos ['Ingredientes'] = null;
+        }
     })
+
+    const nutrientes = document.querySelector('#panel_nutrition_facts_table_content')
+    if(nutrientes){
+        const tbody = nutrientes.querySelector('tbody');
+        
+        if(tbody){
+            const linhas = tbody.querySelectorAll('tr');
+            const arrayNutrientes = [];
+            linhas.forEach(linha=>{
+                const spans = linha.querySelectorAll('td > span');
+                if (spans){
+                    
+                        const chave = spans[0].innerText.trim();
+                        const valor = spans[1].innerText.trim();
+                        const nutriente = {};
+                        nutriente [chave] = valor;
+                        arrayNutrientes.push(nutriente);
+                                   
+                }else{
+                    elementos [null] = null
+                }
+            })
+            elementos['Nutrientes'] = arrayNutrientes
+        }
+    }
+
     
     console.log(elementos)
 
